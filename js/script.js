61f68499c80280aecc27e5744132fabde17c79cc
mijanur34 <smmijanur34@gmail.com>
2024-11-09 13:36:00 +0600

    modified3

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ function showDateTime() {
 }
 // Update the time every second
 setInterval(showDateTime, 1000);
+function toggleDropdown(element) {
+    // Close other dropdowns
+    const dropdowns = document.querySelectorAll('.block1 > ul > li');
+    dropdowns.forEach((dropdown) => {
+      if (dropdown !== element) {
+        dropdown.classList.remove('active');
+      }
+    });
+
+    // Toggle the clicked dropdown
+    element.classList.toggle('active');
+  }
